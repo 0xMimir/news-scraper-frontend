5f3d4a4aa5801dd28072f8a96230faa05d69446d
0xMimir <tmimir@proton.me>
2022-11-22 00:08:39 +0100

    bugfixes

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,6 @@ pub mod store;
 pub mod app;
 pub mod components;
 pub mod routes;
-use dotenv::dotenv;
 
 use wasm_bindgen::prelude::*;
 
@@ -18,7 +17,6 @@ static ALLOC: std::alloc::System = std::alloc::System;
 // This is the entry point for the web app
 #[wasm_bindgen]
 pub fn run() -> Result<(), JsValue> {
-    dotenv().ok().unwrap();
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<App>();
     Ok(())
